8ae9d704c5489c888e5d004e2dc89f262b462a83
timzand <Tim Sand>
2021-01-07 08:46:00 +0100

    2nd commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var gridX = 100;
+var gridY = 100;
+var gridArray = [];
+
+function main() {
+    generateGrid();
+}
+
+function generateGrid() {
+    console.log("Generating grid");
+    var grid = document.getElementById("container_grid");
+    grid.innerHTML += "<div class='gridbox'></div>"
+}
+
+
